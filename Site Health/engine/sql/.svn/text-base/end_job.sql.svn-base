643ef14eef2b045b4f2dfdda384e4dd9bf3d0f78
Shantanu Jha <shantanu.jha@pfizer.com>
2021-07-18 20:38:55 +0530

    Removed Site health

--- Site Health/engine/sql/.svn/text-base/end_job.sql.svn-base
+++
@@ -1,3 +0,0 @@
-whenever sqlerror exit sql.sqlcode;
-EXECUTE HM_DM_OWNER.ETL_BATCH_RUN_CTRL_API.END_JOB('&1') 
-exit;
--- Site Health/engine/sql/fail_job.sql
+++
@@ -1,3 +0,0 @@
-whenever sqlerror exit sql.sqlcode;
-EXECUTE HM_DM_OWNER.ETL_BATCH_RUN_CTRL_API.FAIL_JOB('&1', '&2') 
-exit;
